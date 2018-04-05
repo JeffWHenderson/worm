8fb69cfc4bd851e713d7efc608bead62f18ddefe
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-05 14:13:34 -0400

    made some really redundant conditionals. These youtube game making tuts are pretty funny. Its interesting how many different ways you can make code work... even if it REALLY needs refactoring.

--- src/com/jeffwhenderson/snake/GamePlay.java
+++ src/com/jeffwhenderson/snake/GamePlay.java
@@ -102,9 +102,55 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			moves++;
+			right = true;
+			if(!left)
+				right = true;
+			else {
+				right = false;
+				left = true;
+			}
+			up = false;
+			down = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			moves++;
+			left = true;
+			if(!right)
+				left = true;
+			else {
+				left = false;
+				right = true;
+			}
+			up = false;
+			down = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			moves++;
+			up = true;
+			if(!down)
+				up = true;
+			else {
+				up = false;
+				down = true;
+			}
+			left = false;
+			right = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			moves++;
+			down = true;
+			if(!up)
+				down = true;
+			else {
+				down = false;
+				up = true;
+			}
+			left = false;
+			right = false;
+		}
 	}
 
 	@Override
